fc67fc80e3147bc7ea5ab7a610a62ccf35ecf9bc
Pascal Schmiel <pascal.schmiel@gmail.com>
2013-02-20 12:33:32 +0100

    update

--- backup.pl
+++ backup.pl
@@ -4,40 +4,76 @@ use warnings;
 
 # Use the module
 use Mail::IMAPClient;
-use IO::Socket::SSL;
 use Sys::Hostname;
+use Getopt::Std;
 
- my $imap = Mail::IMAPClient->new(
-   Server   => 'imap.gmail.com',
-   User     => 'user.name@gmail.com',
-   Password => 'foobar',
-   Port     => 993,
-   Ssl      => 1,
-  )
-        or die "IMAP Failure: $@";
+sub usage()
+{
+  print STDERR << "EOF";
+-u : IMAP Username
+-p : IMAP Password
+EOF
+  exit;
+}
 
- $imap->State(Mail::IMAPClient::Connected());
+my %options=();
+getopts("u:p:F:", \%options) or usage();
 
- foreach my $box qw( INBOX ) {
+my $mail_password = $options{p} || usage();
+my $mail_user = $options{u} || usage();
+my $mail_box = $options{F} || usage();
 
-   $imap->select($box)
-        or die "IMAP Select Error: $!";
+my $imap = Mail::IMAPClient->new(
+  Server   => 'imap.gmail.com',
+  User     => $mail_user,
+  Password => $mail_password,
+  Port     => 993,
+  Ssl      => 1,
+ )
+       or die "IMAP Failure: $@";
 
-   my @msgs = $imap->search('ALL')
-     or die "Couldn't get all messages\n";
+$imap->State(Mail::IMAPClient::Connected());
 
-   foreach my $msg (@msgs) {
-     my $filename = time . "." . int(rand(10000)) . "." . hostname();
-     open my $fh, ">>mail/$filename"
-       or die("Open File Error: $!");
+if ( !-d 'mail' ) {
+  mkdir('mail',0777);
+}
 
-     $imap->message_to_file($fh, $msg);
+foreach my $box (qw( INBOX )) {
 
-     close $fh
-       or die("Formail Close Pipe Error: $!");
-   }
+  if ( !-d "mail/$box" ) {
+    mkdir("mail/$box",0777);
+  }
 
-   $imap->close($box);
- }
+  if ( !-d "mail/$box/cur" ) {
+    mkdir("mail/$box/cur",0777);
+  }
 
- $imap->logout();
+  if ( !-d "mail/$box/new" ) {
+    mkdir("mail/$box/new",0777);
+  }
+
+  if ( !-d "mail/$box/tmp" ) {
+    mkdir("mail/$box/tmp",0777);
+  }
+
+  $imap->select($box)
+       or die "IMAP Select Error: $!";
+
+  my @msgs = $imap->search('ALL')
+    or die "Couldn't get all messages\n";
+
+  foreach my $msg (@msgs) {
+    my $filename = time . "." . int(rand(10000)) . "." . hostname();
+    open my $fh, ">>mail/$box/cur/$filename"
+      or die("Open File Error: $!");
+
+    $imap->message_to_file($fh, $msg);
+
+    close $fh
+      or die("Formail Close Pipe Error: $!");
+  }
+
+  $imap->close($box);
+}
+
+$imap->logout();
